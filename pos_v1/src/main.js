d61b490d349dea4f8971050632742d633e703208
zhangxv <raingxm@163.com>
2014-10-27 18:55:17 +0800

    get item count from input

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,9 +1,44 @@
 //TODO: Please write code in this file.
-function printInventory(inputCart) {
+function printInventory(inputs) {
 
-
-
+  console.log(getItemsCountMapFromInput(inputs)["ITEM000001"] + "");
 }
+
+function getItemsList() {
+  var itemsInfo = "";
+
+  // for(var key in goodMap) {
+  //   itemsInfo += getItemList(key, goodMap[key]);
+  // }
+  //
+  // return itemsInfo;
+}
+
+function getItemsCountMapFromInput(inputCart) {
+  var goodMap = {};
+  var barcode = "";
+  var amount = 0;
+  for(var i = 0; i < inputCart.length; i++) {
+    if(isWeighingGood(inputCart[i])) {
+      barcode = getWeighingGoodStandardBarcode(inputCart[i]);
+      amount = getWeighingGoodAmount(inputCart[i]);
+      goodMap[barcode] = amount;
+      continue;
+    }
+
+    barcode = inputCart[i];
+    if(goodMap[barcode]) {
+      goodMap[barcode]++;
+    }else {
+      goodMap[barcode] = 1;
+    }
+  }
+  return goodMap;
+}
+
+// function getItemList(barcode, amount) {
+//   return barcode + amount;
+// }
 
 function getItemByBarcode(barcode) {
   var allItems = loadAllItems();
@@ -35,6 +70,6 @@ function getWeighingGoodAmount(inputItem) {
   return parseInt(inputItem.split('-')[1]);
 }
 
-function getWeighingGoodBarcode(inputItem) {
+function getWeighingGoodStandardBarcode(inputItem) {
   return inputItem.split('-')[0];
 }
