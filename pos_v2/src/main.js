dd52c086829bc2933c8cc492e80372fa15634d6f
zhangxv <raingxm@163.com>
2014-11-04 10:16:05 +0800

    move some function from main.js to shoppingCart

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -11,35 +11,7 @@ var dottedLine = function() {
   return "----------------------\n";
 };
 
-var isWeighingGood = function(inputItem) {
-  return inputItem.indexOf("-") != -1;
-};
 
-var weighingGoodAmount = function(inputItem) {
-  return parseInt(inputItem.split('-')[1]);
-};
-
-var weighingGoodBarcode = function(inputItem) {
-  return inputItem.split('-')[0];
-};
-
-var getItemsCountMapFromInput = function(inputs) {
-  var itemsCountMap = {};
-  for(var i = 0; i < inputs.length; i++) {
-    if(isWeighingGood(inputs[i])) {
-      var barcode = weighingGoodBarcode(inputs[i]);
-      itemsCountMap[barcode] = weighingGoodAmount(inputs[i]);
-      continue;
-    }
-
-    if(itemsCountMap.hasOwnProperty(inputs[i])) {
-      itemsCountMap[inputs[i]]++;
-    } else {
-      itemsCountMap[inputs[i]] = 1;
-    }
-  }
-  return itemsCountMap;
-};
 
 var printInventory = function(inputs) {
   var xuebi = new LineItem("雪碧", 3, 2, "瓶");
--- pos_v2/src/model/shoppingCart.js
+++ pos_v2/src/model/shoppingCart.js
@@ -1,4 +1,5 @@
-var ShoppingCart = function() {
+var ShoppingCart = function(inputs) {
+  this.inputs = inputs;
   this.collection = [];
 };
 
@@ -8,4 +9,34 @@ ShoppingCart.prototype.add = function(lineItem) {
 
 ShoppingCart.prototype.count = function() {
   return this.collection.length;
+};
+
+ShoppingCart.prototype.getItemsCountMapFromInput = function() {
+  var itemsCountMap = {};
+  for(var i = 0; i < inputs.length; i++) {
+    if(isWeighingGood(inputs[i])) {
+      var barcode = weighingGoodBarcode(inputs[i]);
+      itemsCountMap[barcode] = weighingGoodAmount(inputs[i]);
+      continue;
+    }
+
+    if(itemsCountMap.hasOwnProperty(inputs[i])) {
+      itemsCountMap[inputs[i]]++;
+    } else {
+      itemsCountMap[inputs[i]] = 1;
+    }
+  }
+  return itemsCountMap;
+};
+
+ShoppingCart.prototype.isWeighingGood = function(inputItem) {
+  return inputItem.indexOf("-") != -1;
+};
+
+ShoppingCart.prototype.weighingGoodAmount = function(inputItem) {
+  return parseInt(inputItem.split('-')[1]);
+};
+
+ShoppingCart.prototype.weighingGoodBarcode = function(inputItem) {
+  return inputItem.split('-')[0];
 };
