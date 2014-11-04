55a0db841247bc23ee68580d8af2295390c19505
zhangxv <raingxm@163.com>
2014-11-04 10:02:13 +0800

    add a object file to descript customer's shopping cart

---
+++ pos_v2/src/model/shoppingCart.js
@@ -0,0 +1,11 @@
+var ShoppingCart = function() {
+  this.collection = [];
+};
+
+ShoppingCart.prototype.add = function(lineItem) {
+  this.collection.push(lineItem);
+};
+
+ShoppingCart.prototype.count = function() {
+  return this.collection.length();
+};
