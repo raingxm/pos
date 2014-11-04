1dd9d066613dc94ae7570566a39bbe258590c79c
zhangxv <raingxm@163.com>
2014-11-04 08:56:23 +0800

    add three function to print title, stars line and dotted line in shopping list

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -1,6 +1,18 @@
 
-var printInventory = function() {
+var title = function() {
+  return "***<没钱赚商店>购物清单***\n";
+};
 
+var starsLine = function() {
+  return "**********************";
+};
+
+var dottedLine = function() {
+  return "----------------------\n";
+};
+
+var printInventory = function() {
+  console.log(title() + dottedLine() + starsLine());
 };
 
 printInventory();
